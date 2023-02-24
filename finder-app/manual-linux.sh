64f1960347bc57253de74395baabf79a14798a1b
Ryan Birmingham <birm@rbirm.us>
2023-02-24 04:08:24 -0500

    more make for .conf err

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,12 +35,15 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+		make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
 		make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
 		#make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
 		make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
